7b70a2d680ed604f23d6317a4a25ed4a2479f954
vvronskyFX <vronskymail@gmail.com>
2021-12-08 23:10:23 -0500

    first commit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,27 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+const cryptoApiHeaders = {
+	'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+	'x-rapidapi-key': 'b17528cf86msh59d8aa98bf412ebp186b08jsnf7559b8f1cf3'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+	reducerPath: "cryptoApi",
+	baseQuery: fetchBaseQuery({ baseUrl }),
+	endpoints: (builder) => ({
+		getCryptos: builder.query({
+			query: (count) => createRequest(`/coins?limit=${count}`),
+		}),
+		getCryptoDetails: builder.query({
+			query: (coinId) => createRequest(`/coin/${coinId}`),
+		})
+	})
+});
+
+export const {
+	useGetCryptosQuery, useGetCryptoDetailsQuery
+} = cryptoApi;
